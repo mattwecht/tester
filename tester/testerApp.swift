41a1f99b93d42b1cb8431a7be0a0db091900e5e7
Matt Wecht <mattwecht@gmail.com>
2023-06-26 15:32:37 -0400

    Initial Commit

---
+++ tester/testerApp.swift
@@ -0,0 +1,17 @@
+//
+//  testerApp.swift
+//  tester
+//
+//  Created by scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct testerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
